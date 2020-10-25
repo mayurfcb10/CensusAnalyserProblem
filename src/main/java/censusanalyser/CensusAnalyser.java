399143138ba2654f447c1efbec6223cc05eedc1b
Mayur <mayurzope555@gmail.com>
2020-10-25 11:51:37 +0530

    [Mayur]Added: Method to load Common CSV states and india census data

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -6,15 +6,18 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.stream.StreamSupport;
-import com.bridgelabz.censusanalyser.*;
 
-//import com.bridgelabz.censusanalyser.*;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import com.bridgelabz.censusanalyser.*;
 
 public class CensusAnalyser {
 	public void welcomeMessage() {
 		System.out.println("Welcome to the Census Analyser Problem");
 	}
 
+	// Reading Open CSV file to load india census data 
 	public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
 			ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
@@ -26,12 +29,28 @@ public class CensusAnalyser {
 		}
 	}
 
+	// Reading Open CSV file to load state data 
 	public int loadIndianStateCode(String csvFilePath) throws CensusAnalyserException {
 		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
 			ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
 			Iterator<IndiaCensusCSV> stateCSVIterator = csvBuilder.getCSVFileIterator(reader, CSVStates.class);
 			return getNumOfEntries(stateCSVIterator);
 		} catch (IOException | CSVBuilderException e) {
+			throw new CensusAnalyserException(e.getMessage(),
+					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+		}
+	}
+
+	// Reading Common CSV file to load status and india census data 
+	public int loadIndiaStateOrCensusDataUsingCommonsCSV(String csvFilePath) throws CensusAnalyserException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+			Iterator<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader()
+					.withIgnoreHeaderCase()
+					.withTrim()
+					.parse(reader)
+					.iterator();
+			return this.getNumOfEntries(records);
+		} catch (IOException | RuntimeException e) {
 			throw new CensusAnalyserException(e.getMessage(),
 					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
 		}
